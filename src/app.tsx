5b7649701118784e14d87bb4d57dec35deba7366
corbanesi <alessandrocorbanesi@gmail.com>
2024-05-26 15:33:04 -0300

    feat: before unload message

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
 import { TierList } from "@/components/tier-list";
 
 function App() {
+	useEffect(() => {
+		const handleUnload = (event: BeforeUnloadEvent) => {
+			event.preventDefault();
+		};
+		window.addEventListener("beforeunload", handleUnload, { capture: true });
+		return () => {
+			window.removeEventListener("beforeunload", handleUnload);
+		};
+	}, []);
+
 	return (
 		<>
 			<div className="flex min-h-screen px-2 container mx-auto">
