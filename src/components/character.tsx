fece9b5c1d6418387a6ea1fcc3e9b96fb9fcc916
corbanesi <alessandrocorbanesi@gmail.com>
2024-07-01 20:59:40 -0300

    fixed icons when mobile

--- src/components/character.tsx
+++ src/components/character.tsx
@@ -36,9 +36,9 @@ export function Character(props: Props) {
 				className="h-16 w-16 md:h-24 md:w-24 print:w-16 print:h-16 touch-none border border-slate-400 bg-slate-50"
 			/>
 			<div className="flex gap-1 justify-center pt-1">
-				<img src={attrData.find((attr) => attr.name === character.attr)?.img} className="h-6 w-6 print:h-4 print:w-4" alt="char attr" />
-				<img src={tierData.find((tier) => tier.name === character.tier)?.img} className="h-6 w-6 print:h-4 print:w-4" alt="char tier" />
-				<img src={typeData.find((type) => type.name === character.type)?.img} className="h-6 w-6 print:h-4 print:w-4" alt="char type" />
+				<img src={attrData.find((attr) => attr.name === character.attr)?.img} className="h-4 w-4 md:h-6 md:w-6 print:h-4 print:w-4" alt="char attr" />
+				<img src={tierData.find((tier) => tier.name === character.tier)?.img} className="h-4 w-4 md:h-6 md:w-6 print:h-4 print:w-4" alt="char tier" />
+				<img src={typeData.find((type) => type.name === character.type)?.img} className="h-4 w-4 md:h-6 md:w-6 print:h-4 print:w-4" alt="char type" />
 			</div>
 		</div>
 	);
